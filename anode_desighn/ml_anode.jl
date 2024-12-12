4efdda5ad564e18fa57e36cc26c0c5c0be3e6165
AidanSgar <39351316+AidanSgar@users.noreply.github.com>
2024-12-12 12:33:25 -0800

    still not working

--- anode_desighn/ml_anode.jl
+++ anode_desighn/ml_anode.jl
@@ -7,49 +7,66 @@ function potential(points::Vector{MVector{2, Float64}})
 	N = length(points)
 	U = 0.0
 	for i in 1:N-1
+		ϕ1, θ1 = points[i]
 		for j in i+1:N
-			ϕ1, θ1 = points[i]
 			ϕ2, θ2 = points[j]
-			d      = acos(sin(ϕ1) * sin(ϕ2) + cos(ϕ1) * cos(ϕ2) * cos(θ1 - θ2))
-			U      += 1 / d + 1 / (π - d) - 4/π
+			d = acos(sin(ϕ1) * sin(ϕ2) + cos(ϕ1) * cos(ϕ2) * cos(θ1 - θ2))
+			U += 1 / d + 1 / (π - d) - 4/π
 		end
 	end
 
 	return U
 end
 
-function grad_spherical(i::Int, points::Vector{MVector{2, Float64}})::MVector{2, Float64}
+function grad_a(i::Int, points::Vector{MVector{2, Float64}})::MVector{2, Float64}
 	ϕ1, θ1 = points[i]
 	sϕ1, cϕ1 = sincos(ϕ1)
-	Sϕ1, Sϕ2, Sθ = 0.0, 0.0, 0.0
-
+	dϕ, dθ = 0.0, 0.0
 	for j in 1:length(points)
 		if i != j
 			ϕ2, θ2 = points[j]
 			sϕ2, cϕ2 = sincos(ϕ2)
-			cθ = cos(θ1 - θ2)
+			sθ, cθ = sincos(θ1 - θ2)
 			d = acos(sϕ1 * sϕ2 + cϕ1 * cϕ2 * cθ)
 			val = (1 / (π - d)^2 - 1 / d^2) / sin(d)
-			Sϕ1 += cθ * cϕ2 * val
-			Sϕ2 += sϕ2 * val
-			Sθ += sin(θ1 - θ2) * cϕ2 * val
+			dϕ += (sϕ1 * cϕ2 * cθ - cϕ1 * sϕ2) * val
+			dθ += cϕ2 * sθ * val
 		end
 	end
 
-	return MVector(Sϕ1 * sϕ1 - Sϕ2 * cϕ1, Sθ * cϕ1 / sϕ1^2)
+	return MVector(dϕ, (cϕ1 / sϕ1) * dθ)
 end
 
-function evolve!(points::Vector{MVector{2, Float64}}; γ = 1e-2, min_grad = 1e-6, max_iter = 1_000_000)
+function grad_n(i::Int, points::Vector{MVector{2, Float64}}; h = 1e-8)::MVector{2, Float64}
+	points[i][1] += h
+	pos_ϕ = potential(points)
+	points[i][1] -= 2*h
+	neg_ϕ = potential(points)
+	points[i][1] += h
+	dϕ = (pos_ϕ - neg_ϕ)/(2*h)
+
+	points[i][2] += h
+	pos_θ = potential(points)
+	points[i][2] -= 2*h
+	neg_θ = potential(points)
+	points[i][2] += h
+	dθ = (pos_θ - neg_θ)/(2*h)
+	dθ /= sin(points[i][1])
+
+	return MVector(dϕ, dθ)
+end
+
+function evolve!(points::Vector{MVector{2, Float64}}; γ = 1e-4, min_grad = 1e-12, max_iter = 10_000_000)
 	N = length(points)
 	iter = 0
 	grad_norm = Inf
 	while (iter < max_iter) && (grad_norm > min_grad)
 		grad_norm = 0.0
-		grad = [grad_spherical(i, points) for i in 1:N]
+		grad = [grad_a(i, points) for i in 1:N]
 		for (p, g) in zip(points, grad)
-			grad_norm += g[1]^2 + sin(p[1])^2 * g[2]^2
-			p[1] += -γ * g[1]
-			p[2] += -γ * g[2]
+			grad_norm += g[1]^2 + g[2]^2
+			p[1] -= γ * g[1]
+			p[2] -= γ * g[2]
 
 			p[1] = mod(p[1], 2 * π)
 			p[2] = mod(p[2], 2 * π)
@@ -58,6 +75,7 @@ function evolve!(points::Vector{MVector{2, Float64}}; γ = 1e-2, min_grad = 1e-6
 				p[2] = mod(p[2] + π, 2 * π)
 			end
 		end
+		iter += 1
 	end
 end
 
@@ -86,7 +104,7 @@ function embed_points_R3(points::Vector{MVector{2, Float64}})
 	end
 
 	z = [0.0, 0.0, 1.0]
-	_, idx = findmin([norm(v .- z)v for v in vecs])
+	_, idx = findmin([norm(v .- z) for v in vecs])
 	v = vecs[idx]
 	point_mat = hcat(vecs...)
 
