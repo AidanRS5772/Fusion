c2d7fefc4f5d52a9b7a1d586820d7cf63db279b7
AidanSgar <39351316+AidanSgar@users.noreply.github.com>
2024-12-23 13:48:04 -0800

    changed orbit cnt

--- anode_desighn/anode_sim.jl
+++ anode_desighn/anode_sim.jl
@@ -12,6 +12,17 @@ using Distributions
 using Statistics
 using JSON
 
+function cube_layout(lim)
+    return Layout(
+        scene=attr(
+            aspectmode="cube",
+            xaxis=attr(range=[-lim, lim]),
+            yaxis=attr(range=[-lim, lim]),
+            zaxis=attr(range=[-lim, lim])
+        )
+    )
+end
+
 function plot_path(mesh, path)
     # For each triangle, get its three vertices
     x = Float64[]
@@ -67,9 +78,9 @@ end
 
 function scale_mesh(mesh, scale_factor)
     verts = coordinates(mesh)
-
+    b_radius = maximum([norm(v.position) for v in verts])
     new_vertices = StructVector{typeof(verts[1])}((
-        position=[Point{3,Float32}(v.position .* scale_factor) for v in verts],
+        position=[Point{3,Float32}(v.position .* scale_factor ./ b_radius) for v in verts],
         normals=verts.normals
     ))
 
@@ -139,41 +150,42 @@ function intersect(p1, p2, v1, v2, v3)
     return false
 end
 
-function find_path(mesh, prox_map, sphere_radius, max_pass, E, B, u0)
-    pass_cnt = 0
-    p2 = u0[1:3]
-    r2 = norm(p2)
+function find_path(mesh, prox_map, sphere_radius, max_orbit, E, u0)
+    orbit_cnt = 0
+    p0 = u0[1:3]
+    p1 = u0[1:3]
+    n0 = norm(p1)
+    n1 = n0
     function condition(u, t, integrator)
-        p1 = u[1:3]
-        r1 = norm(p1)
-        if (r1 <= sphere_radius <= r2) || (r2 <= sphere_radius <= r1)
-            pass_cnt += 1
-            if pass_cnt >= max_pass
+        p = u[1:3]
+        n = norm(u[1:3])
+        if n0 < n1 && n < n1 && sphere_radius < n1
+            orbit_cnt += 1
+            if orbit_cnt >= max_orbit
                 return true
             end
         end
 
-        valid_idxs = unique!(vcat(prox_map.cells[bounding_box_idxs(p1, p2, bound=prox_map.bound, N=prox_map.N)]...))
+        valid_idxs = unique!(vcat(prox_map.cells[bounding_box_idxs(p, p1, bound=prox_map.bound, N=prox_map.N)]...))
         @inbounds for j in valid_idxs
             v1 = SVector{3}(mesh[j][1].position)
             v2 = SVector{3}(mesh[j][2].position)
             v3 = SVector{3}(mesh[j][3].position)
-            if intersect(p2, p1, v1, v2, v3)
+            if intersect(p, p1, v1, v2, v3)
                 return true
             end
         end
 
-        p2 = p1
-        r2 = r1
+        p0, p1, n0, n1 = p1, p, n1, n
+
         return false
     end
 
     cb = DiscreteCallback(condition, terminate!)
-
-    prob = ODEProblem(lorenz!, u0, (0.0, 1.0), (E, B))
+    prob = ODEProblem(lorenz!, u0, (0.0, 1.0), (E))
     sol = solve(prob, Rodas5P(autodiff=false), callback=cb)
 
-    return pass_cnt, sol
+    return orbit_cnt, sol
 end
 
 function centroid_areas(mesh)
@@ -205,11 +217,9 @@ end
 
 @fastmath function lorenz!(du, u, p, t)
     e_m = 47917944.84
-    E, B = p
-    r = u[1:3]
-    dr = u[4:6]
-    du[1:3] = dr
-    du[4:6] = e_m .* (E(r) .+ cross(dr, B(r)))
+    E = p
+    du[1:3] = u[4:6]
+    du[4:6] = e_m .* E(u[1:3])
 end
 
 function make_point(u, r, R, T)
@@ -234,56 +244,47 @@ function do_analysis(r, R, V, T, app_cnt, N_cell, N_samples, max_orbit)
     prox_map_intersection = make_prox_map_intersection(mesh, N_cell)
     c, a = centroid_areas(mesh)
     E_feild(ρ) = E(ρ, c, a, r, R, V)
-    B_feild(_) = SVector{3,Float64}(0.0, 0.0, 0.0)
 
     initials = Vector{MVector{6}}(undef, N_samples)
-    pass_cnts = Vector{Int}(undef, N_samples)
+    orbit_cnts = Vector{Int}(undef, N_samples)
     uni = rand(6 * N_samples)
     print("0%")
     @inbounds for i in 1:N_samples
         u0 = make_point(uni[6*i-5:6*i], r, R, T)
         initials[i] = u0
-        pass_cnts[i], _ = find_path(mesh, prox_map_intersection, r, 2 * max_orbit + 1, E_feild, B_feild, u0)
+        orbit_cnts[i], _ = find_path(mesh, prox_map_intersection, r, max_orbit, E_feild, u0)
         print("\r$(round(i*100/N_samples, digits = 2))%")
     end
 
-    full_orbits = []
-    partial_orbit_cnt = 0
-    eternal_initials = []
-    for i in 1:N_samples
-        orbit_cnt = pass_cnts[i] ÷ 2
-        if orbit_cnt != max_orbit
-            push!(full_orbits, orbit_cnt)
-        else
-            push!(eternal_initials, initials[i])
-        end
-        if pass_cnts[i] % 2 == 0
-            partial_orbit_cnt += 1
-        end
-    end
-
     pdf_trace = histogram(
-        x=full_orbits,
+        x=orbit_cnts,
         opacity=0.5,
         nbinsx=max_orbit,
         histnorm="probability",
         marker_color="rgb(100, 150, 200)"
     )
 
-    no_pass_rate = partial_orbit_cnt / sum(full_orbits)
-    println("\nNo Pass Rate: ", no_pass_rate)
-    mean_orbit = mean(full_orbits)
+    eternal_initials = []
+    del_idxs = []
+    for i in eachindex(orbit_cnts)
+        if orbit_cnts[i] == max_orbit
+            push!(del_idxs, i)
+            push!(eternal_initials, initials[i])
+        end
+    end
+    deleteat!(orbit_cnts, del_idxs)
+
+    mean_orbit = mean(orbit_cnts)
     println("Mean Orbit Count: ", mean_orbit)
 
-    full_orbits .+= 1
-    α, β = Distributions.params(fit(Gamma, Float64.(full_orbits)))
-    #println("α: $(α)")
-    #println("β: $(β)")
+    α, β = Distributions.params(fit(Gamma, Float64.(orbit_cnts)))
+    println("α: $(α)")
+    println("β: $(β)")
 
-    P_expected_tail = 1 - cdf(Gamma(α, β), max_orbit + 1)
+    P_expected_tail = 1 - cdf(Gamma(α, β), max_orbit)
     P_observed_tail = length(eternal_initials) / N_samples
     eternal_prop = P_observed_tail - P_expected_tail
-    #println("Proportionl of Phase Space with an Eternal Path: ", eternal_prop)
+    println("Proportionl of Phase Space with an Eternal Path: ", eternal_prop)
 
     file = "anode_data/appratures_$(app_cnt).json"
     data = JSON.parsefile(file)
@@ -292,7 +293,6 @@ function do_analysis(r, R, V, T, app_cnt, N_cell, N_samples, max_orbit)
     data["β"] = β
     data["eternal_prop"] = eternal_prop
     data["eternal_initials"] = eternal_initials
-    data["no_pass_rate"] = no_pass_rate
 
     open(file, "w") do f
         JSON.print(f, data)
@@ -306,7 +306,7 @@ end
 r, R, V, T = 0.05, 0.5, 1e5, 300
 N_cell = 20
 N_samples = 100
-max_orbit = 50
+max_orbit = 10
 app_cnt = 6
 
 
@@ -314,12 +314,11 @@ mesh = scale_mesh(load("anode_meshes/appratures_$(app_cnt).stl"), r)
 prox_map_intersection = make_prox_map_intersection(mesh, N_cell)
 c, a = centroid_areas(mesh)
 E_feild(ρ) = E(ρ, c, a, r, R, V)
-B_feild(_) = SVector{3,Float64}(0.0, 0.0, 0.0)
 
 u0 = make_point(rand(6), r, R, T)
-pass_cnt, sol = find_path(mesh, prox_map_intersection, r, 2 * max_orbit, E_feild, B_feild, u0)
+orbit_cnt, sol = find_path(mesh, prox_map_intersection, r, max_orbit, E_feild, u0)
 
-println(pass_cnt)
+println(orbit_cnt)
 
-sol = hcat(sol.u...)
-plot(plot_path(mesh, scale))
+sol = hcat(Vector.(sol.u)...)
+plot(plot_path(mesh, sol), cube_layout(R))
